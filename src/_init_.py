bd02d7d9baad3ae0310f3b0c04ab3ded85ba8e48
gabi20-1k9j0y7 <144603383+gabi20-1k9j0y7@users.noreply.github.com>
2025-12-08 21:34:40 +0100

    Update _init_.py

--- src/_init_.py
+++ src/_init_.py
@@ -1,9 +1,1 @@
-"""
-src/utils.py — Utilidades comunes.
-"""
-from pandas import Timestamp
-from pandas.tseries.offsets import BDay
 
-def bday_add(days: int):
-    """Devuelve fecha objetivo sumando días hábiles a hoy."""
-    return (Timestamp.today().normalize() + BDay(days)).date()
